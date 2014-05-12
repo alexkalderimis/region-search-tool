45427ed2b3660f74682bb8f1716357f010de28ea
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-05-12 16:41:44 +0100

    Dealing a bit better with layout.

--- js/input-group.js
+++ js/input-group.js
@@ -9,30 +9,32 @@ define(['react'], function (React) {
     displayName: 'InputGroup',
 
     render: function () {
-      return d.div(
-        {className: 'form-group' + (this.props.className ? ' ' + this.props.className : '')},
+      return d.form(
+        {className: 'form-inline' + (this.props.className ? ' ' + this.props.className : '')},
         d.div(
-          {className: 'input-group'},
-          d.span(
-            {className: 'input-group-addon'}, this.props.name),
-          d.input(
-            {
-              className: 'form-control',
-              ref: 'inputNode',
-              onChange: this._onChange,
-              type: 'text',
-              value: this.props.model,
-              placeholder: this.props.name
-            }),
-          d.span(
-            {className: 'input-group-btn'},
-            d.button(
+          {className: 'form-group'},
+          d.div(
+            {className: 'input-group'},
+            d.span(
+              {className: 'input-group-addon'}, this.props.name),
+            d.input(
               {
-                className: 'btn btn-default',
-                type: 'button',
-                onClick: this.props.onChange.bind(null, "")
-              },
-              d.i({className: 'fa fa-times'})))));
+                className: 'form-control',
+                ref: 'inputNode',
+                onChange: this._onChange,
+                type: 'text',
+                value: this.props.model,
+                placeholder: this.props.name
+              }),
+            d.span(
+              {className: 'input-group-btn'},
+              d.button(
+                {
+                  className: 'btn btn-default',
+                  type: 'button',
+                  onClick: this.props.onChange.bind(null, "")
+                },
+                d.i({className: 'fa fa-times'}))))));
     },
 
     _onChange: function (event) {
